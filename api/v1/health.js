9819e00b6254d422802a3643b0b90ba68c918af6
hanonn0219-lang <hanonn0219@gmail.com>
2025-09-05 17:34:56 +0900

    Create health.js

---
+++ api/v1/health.js
@@ -0,0 +1,7 @@
+export const config = { runtime: 'edge' };
+
+export default async function handler() {
+  return new Response(JSON.stringify({ status: 'ok' }), {
+    headers: { 'Content-Type': 'application/json', 'Cache-Control': 'no-store' }
+  });
+}
