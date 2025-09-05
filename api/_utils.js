f15da1e7b693b661c50d642d3eb0a02174b87dac
hanonn0219-lang <hanonn0219@gmail.com>
2025-09-05 17:35:34 +0900

    Create _utils.js

---
+++ api/_utils.js
@@ -0,0 +1,11 @@
+export function toB64(obj) {
+  return encodeURIComponent(Buffer.from(JSON.stringify(obj)).toString('base64'));
+}
+export function fromB64(str) {
+  return JSON.parse(Buffer.from(decodeURIComponent(str), 'base64').toString('utf8'));
+}
+export function csvEscape(v) {
+  if (v == null) return '';
+  const s = String(v);
+  return /[",\n]/.test(s) ? `"${s.replace(/"/g,'""')}"` : s;
+}
